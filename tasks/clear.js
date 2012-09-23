667aee93177399848cd62b55a75ec5231bf39663
Dave Geddes <davidcgeddes@gmail.com>
2012-09-23 03:40:37 -0600

    clear tasking working

--- tasks/clear.js
+++ tasks/clear.js
@@ -9,12 +9,35 @@
 'use strict';
 
 module.exports = function(grunt) {
-
   // Please see the grunt documentation for more information regarding task
   // creation: https://github.com/cowboy/grunt/blob/master/docs/toc.md
+  grunt.registerTask('clear', 'Clear your terminal window', function() {
+    var child = require('child_process');
+    var ps = child.spawn('clear');
+    ps.stdout.pipe(process.stdout);
+    ps.stdin.end();
 
-  grunt.registerTask('clear', 'Your task description goes here.', function() {
-    grunt.log.write('I am the ' + this.name + ' task...').ok();
+    // grunt.util.spawn({
+    //   cmd: 'clear',
+    //   args: null,
+    //   fallback: ''
+    // // }, function(error, result, code){
+    // }, function(){
+    //   grunt.warn('should have cleared');
+    //   done();
+    // });
+    // var myTerminal = require("child_process").exec,
+    //   commandToBeExecuted = "clear";
+    // myTerminal(commandToBeExecuted, function(error, stdout, stderr) {
+    //   grunt.log.write('should have cleared');
+    //   if (!error) {
+    //     grunt.log.write('cleared ' + this.name + ' task...').ok();    
+    //   }
+    //   else{
+    //     grunt.log.write('failed! ').error();    
+    //   }
+    // });
+    // console.log("\033[2J\033[0f");
+
   });
-
 };
